b905dcaa523af0d99b351ad004b7ae91ff8c8b7a
Ivan Dinkov <ivdinkov@gmail.com>
2017-08-07 00:46:35 +0100

    Lab2 complete

--- Coursera/Solutions/Lab1/Activities/app/src/main/java/ivandinkov/github/com/activities/ActivityB.java
+++ Coursera/Solutions/Lab1/Activities/app/src/main/java/ivandinkov/github/com/activities/ActivityB.java
@@ -9,55 +9,57 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class ActivityB extends AppCompatActivity {
-
+    
     private static final String TAG = "Activity";
-
+    
     protected void onStart(){
         super.onStart();
         Log.i(TAG, "onStart: Activity B is visible and is about to start");
     }
-
+    
     protected void onRestart(){
         super.onRestart();
         Log.i(TAG, "onRestart: Activity B is visible and about to restart");
     }
-
+    
     protected void onResume(){
         super.onResume();
         Log.i(TAG, "onResume: Activity B is and has focus (it is resumed)");
     }
-
+    
     protected void onPause(){
         super.onPause();
         Log.i(TAG, "onPause: Activity B is taking focus, and is about to be paused");
     }
-
+    
     protected void onStop(){
         super.onStop();
         Log.i(TAG, "onStop: Activity B is no longer visible and it is now (stopped)");
     }
-
+    
     protected void onDestroy(){
         super.onDestroy();
         Log.i(TAG, "onDestroy: Activity B is about to be destroyed");
     }
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
+        
+        
         //Load any saved instances
         super.onCreate(savedInstanceState);
-
+        
         //Set content view
         setContentView(R.layout.activity_b);
-
+        
+        Log.i(TAG, "onCreate: ActivityB is created");
+        
         //Initialize TextView
         final TextView activityA = (TextView) findViewById(R.id.textViewActivityB);
-
+        
         //Initialize button
         final Button button = (Button) findViewById(R.id.btnStartActivityA);
-
+        
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- Coursera/Solutions/Lab1/Activities/app/src/main/java/ivandinkov/github/com/activities/MainActivity.java
+++ Coursera/Solutions/Lab1/Activities/app/src/main/java/ivandinkov/github/com/activities/MainActivity.java
@@ -9,71 +9,73 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "Activity";
-
-    protected void onStart(){
-        super.onStart();
-        Log.i(TAG, "onStart: Main Activity is visible and is about to start");
-    }
-
-    protected void onRestart(){
-        super.onRestart();
-        Log.i(TAG, "onRestart: Main Activity is visible and about to restart");
-    }
-
-    protected void onResume(){
-        super.onResume();
-        Log.i(TAG, "onResume: Main Activity is and has focus (it is resumed)");
-    }
-
-    protected void onPause(){
-        super.onPause();
-        Log.i(TAG, "onPause: Main Activity is taking focus, and is about to be paused");
-    }
-
-    protected void onStop(){
-        super.onStop();
-        Log.i(TAG, "onStop: Main Activity is no longer visible and it is now (stopped)");
-    }
-
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.i(TAG, "onDestroy: Main Activity is about to be destroyed");
-    }
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        //Display any saved instance
-        super.onCreate(savedInstanceState);
-
-        //Set view
-        setContentView(R.layout.activity_main);
-
-        //Initialize TextView
-        final TextView activityA = (TextView) findViewById(R.id.textViewActivityA);
-
-        //Initialize button
-        final Button button = (Button) findViewById(R.id.btnStartActivityB);
-        button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                try {
-                    Intent openActivityB = new Intent(MainActivity.this,ActivityB.class);
-                    startActivity(openActivityB);
-            } catch (Exception e){
-                Log.e(TAG,"Unable to start " +
-                        "Activity B");
-            }
-            }
-        });
-
+  
+  private static final String TAG = "Activity";
+  
+  protected void onStart(){
+    super.onStart();
+    Log.i(TAG, "onStart: Main Activity is visible and is about to start");
+  }
+  
+  protected void onRestart(){
+    super.onRestart();
+    Log.i(TAG, "onRestart: Main Activity is visible and about to restart");
+  }
+  
+  protected void onResume(){
+    super.onResume();
+    Log.i(TAG, "onResume: Main Activity is and has focus (it is resumed)");
+  }
+  
+  protected void onPause(){
+    super.onPause();
+    Log.i(TAG, "onPause: Main Activity is taking focus, and is about to be paused");
+  }
+  
+  protected void onStop(){
+    super.onStop();
+    Log.i(TAG, "onStop: Main Activity is no longer visible and it is now (stopped)");
+  }
+  
+  protected void onDestroy(){
+    super.onDestroy();
+    Log.i(TAG, "onDestroy: Main Activity is about to be destroyed");
+  }
+  
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    
+    //Display any saved instance
+    super.onCreate(savedInstanceState);
+    
+    //Set view
+    setContentView(R.layout.activity_main);
+    
+    Log.i(TAG, "onCreate: Main Activity is created");
+    
+    //Initialize TextView
+    final TextView activityA = (TextView) findViewById(R.id.textViewActivityA);
+    
+    //Initialize button
+    final Button button = (Button) findViewById(R.id.btnStartActivityB);
+    button.setOnClickListener(new View.OnClickListener() {
+      
+      @Override
+      public void onClick(View v) {
+        try {
+          Intent openActivityB = new Intent(MainActivity.this,ActivityB.class);
+          startActivity(openActivityB);
+        } catch (Exception e){
+          Log.e(TAG,"Unable to start " +
+                  "Activity B");
         }
-
-    }
+      }
+    });
+    
+  }
+  
+}
 
 
 
