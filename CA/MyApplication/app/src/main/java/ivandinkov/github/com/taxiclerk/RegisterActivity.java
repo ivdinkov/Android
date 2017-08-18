7af3f40258719ff4c0303df2de1972ea480251d1
ivdinkov <ivdinkov@gmail.com>
2017-08-18 15:09:39 +0100

    Register activity initial UI ready

---
+++ CA/MyApplication/app/src/main/java/ivandinkov/github/com/taxiclerk/RegisterActivity.java
@@ -0,0 +1,16 @@
+package ivandinkov.github.com.taxiclerk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class RegisterActivity extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+		
+		
+	}
+}
