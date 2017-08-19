650c0881def9468310dcee0f39f2a08dff301516
ivdinkov <ivdinkov@gmail.com>
2017-08-19 12:31:31 +0100

    Register UI complete

--- CA/MyApplication/app/src/main/java/ivandinkov/github/com/taxiclerk/RegisterActivity.java
+++ CA/MyApplication/app/src/main/java/ivandinkov/github/com/taxiclerk/RegisterActivity.java
@@ -1,8 +1,9 @@
 package ivandinkov.github.com.taxiclerk;
-
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 	
@@ -11,6 +12,17 @@ public class RegisterActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
 		
+		TextView email = (TextView) findViewById(R.id.txtEmail);
+		TextView password = (TextView) findViewById(R.id.txtPass1);
+		TextView button = (TextView) findViewById(R.id.btnRegister);
 		
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO validate user input
+				startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+			}
+		});
 	}
+	
 }
