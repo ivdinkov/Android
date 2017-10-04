1cb42815c2edd8325e6b2ca6479ec50772272bb9
ivdinkov <ivdinkov@gmail.com>
2017-10-04 11:55:10 +0100

    Q2

---
+++ Exam/Q2/app/src/main/java/labs/course/q2/DetailsFragment.java
@@ -0,0 +1,69 @@
+package labs.course.q2;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+//Several Activity and Fragment lifecycle methods are instrumented to emit LogCat output
+//so you can follow the class' lifecycle
+public class DetailsFragment extends Fragment {
+	
+	private static final String TAG = "QuotesFragment";
+	
+	private TextView mQuoteView = null;
+	private int mCurrIdx = -1;
+	private int mQuoteArrLen;
+	
+	public int getShownIndex() {
+		return mCurrIdx;
+	}
+	
+	// Show the Quote string at position newIndex
+	void showQuoteAtIndex(int newIndex) {
+		if (newIndex < 0 || newIndex >= mQuoteArrLen)
+			return;
+		mCurrIdx = newIndex;
+		mQuoteView.setText(GotFamilyViewerActivity.mQuoteArray[mCurrIdx]);
+	}
+	
+	@Override
+	public void onAttach(Activity activity) {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onAttach()");
+		super.onAttach(activity);
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onCreate()");
+		super.onCreate(savedInstanceState);
+	}
+	
+	// Called to create the content view for this Fragment
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+													 Bundle savedInstanceState) {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onCreateView()");
+		
+		// Inflate the layout defined in quote_fragment.xml
+		// The last parameter is false because the returned view does not need to be attached to the container ViewGroup
+		return inflater.inflate(R.layout.fragment_house,
+						container, false);
+	}
+	
+	// Set up some information about the mQuoteView TextView
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onActivityCreated()");
+		super.onActivityCreated(savedInstanceState);
+		
+		mQuoteView = (TextView) getActivity().findViewById(R.id.quoteView);
+		mQuoteArrLen = GotFamilyViewerActivity.mQuoteArray.length;
+	}
+	
+	
+}
